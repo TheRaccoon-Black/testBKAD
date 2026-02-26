154c0c5965e15271ca7ed1b0733a6dfe08798fa7
muhammad fachrurrozi <ozjibuwat@gmail.com>
2026-02-26 12:32:01 +0700

    edit(route)

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('laporan.index');
 });
 
 Route::get('/laporan', [App\Http\Controllers\LaporanController::class, 'index'])->name('laporan.index');
